4f9506c398e9f2bd8677944fc339196d4a45dddb
Daniel Veleba <daniel.veleba@dtone.com>
2020-04-06 07:58:36 +0200

    test main functionality

--- Gemfile
+++ Gemfile
@@ -7,5 +7,9 @@ gem 'connection_pool', '~> 2.2', '>= 2.2.2'
 gem 'faraday', '~> 1.0', '>= 1.0.1'
 gem 'faraday_middleware', '~> 1.0'
 gem 'oj', '~> 3.10'
-gem 'rspec', '~> 3.9'
 gem 'thread', '~> 0.2.2'
+
+group :development, :test, :ci do
+  gem 'rspec', '~> 3.9'
+  gem 'webmock', '~> 3.8', '>= 3.8.3'
+end
--- spec/lib/main_spec.rb
+++ spec/lib/main_spec.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require 'webmock/rspec'
+
 require_relative '../../lib/main'
 
+# rubocop:disable Metrics/BlockLength
 describe Main do
   context 'for input sanitation' do
     it 'drops commas'
@@ -10,13 +13,46 @@ describe Main do
     it 'removes URL hostile characters'
   end
 
-  it 'works' do
-    # TODO: use HTTP mock adapter to stub out actual calls to the API
-    # Main.run(%w[bulldog-boston  ,  bulldog, xxx  hound bulldog <>#%{}|\^~[]`])
+  # I don't mind creating the files on FS as tests are usually run
+  # in a single-purpose container
+  it 'works for valid args' do
+    stub_request(:get, 'https://dog.ceo/api/breed/bulldog/boston/images')
+      .to_return(body: { message: %w[bb1 bb2], status: 'success' }.to_json,
+                 headers: { 'Content-Type' => 'application/json' })
+    stub_request(:get, 'https://dog.ceo/api/breed/bulldog/images')
+      .to_return(body: { message: %w[b1 b2], status: 'success' }.to_json,
+                 headers: { 'Content-Type' => 'application/json' })
+    stub_request(:get, 'https://dog.ceo/api/breed/hound/images')
+      .to_return(body: { message: %w[h1 h2], status: 'success' }.to_json,
+                 headers: { 'Content-Type' => 'application/json' })
+
+    out_dirs_pre = Dir.glob('out*')
+
+    # rubocop:disable Layout/SpaceInsideArrayPercentLiteral
+    Main.run(%w[bulldog-boston  , bulldog hound bulldog])
+    # rubocop:enable Layout/SpaceInsideArrayPercentLiteral
+
+    out_dirs_post = Dir.glob('out*')
+    dir = (out_dirs_post - out_dirs_pre)[0]
+    expect(Dir.children(dir)).to include('bulldog.csv', 'bulldog-boston.csv',
+                                         'hound.csv', 'updated_at.json')
+
+    # TODO: check file contents
+
+    assert_requested :get, 'https://dog.ceo/api/breed/bulldog/boston/images',
+                     times: 1
+    assert_requested :get, 'https://dog.ceo/api/breed/bulldog/images',
+                     times: 1
+    assert_requested :get, 'https://dog.ceo/api/breed/hound/images',
+                     times: 1
+
+    expect(Main.ret).to eql(0)
   end
 
   context 'returns non-zero return code' do
     it 'for non-existent breeds'
     it 'for API failures'
+    it 'for invalid input' # /<>#%{}|\^~[]`
   end
 end
+# rubocop:enable Metrics/BlockLength
